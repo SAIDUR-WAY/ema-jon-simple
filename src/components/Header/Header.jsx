5a6e9897a7f1503872ebe9c5506d9da3312f2a86
saidur-way <saiduronway@gmail.com>
2023-04-09 12:10:17 +0600

    Active link added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './Header.css';
 import logo from '../../images/Logo.svg'
+import { Link } from 'react-router-dom';
+import ActiveLink from '../ActiveLink/ActiveLink';
 const Header = () => {
      return (
           <nav className='header'>
                <img src={logo} alt="" />
-               <div>
-                    <a href="/">Shop</a>
-                    <a href="/order">Order</a>
-                    <a href="/inventory">Manage Inventory</a>
-                    <a href="/login">Login</a>
+               <div className='link-box'>
+                    <ActiveLink to="/">Shop</ActiveLink>
+                    <ActiveLink to="/order">Order</ActiveLink>
+                    <ActiveLink to="/inventory">Manage Inventory</ActiveLink>
+                    <ActiveLink to="/login">Login</ActiveLink>
                </div>
           </nav>
      );
